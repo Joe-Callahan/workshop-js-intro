7f7de4b401604ab2cceff5b43ae63d6142222cb7
Joe Callahan <joecallahan@macbookair.lan>
2025-01-16 10:06:27 -0800

    create combination page

---
+++ index.js
@@ -0,0 +1,13 @@
+// Display string in an alert box
+
+alert(`You have received this message because you have been chosen to open an important value. Here is the secret combination:`)
+
+// Assign three variables - each using different mathmatical equations
+
+const firstNumber = 110 % 100 //should equal 10
+const secondNumber = 25 + 15 //should equal 40
+const thirdNumber = secondNumber - 1 //should equal 39
+
+// Create code to display combination in an alert box
+
+alert(`The code is: ${firstNumber} - ${secondNumber} - ${thirdNumber}`)
